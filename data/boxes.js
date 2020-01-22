b55ea7da57061b26fd41b8adb978d2caf484bdc4
Logan Scott <logantscott@gmail.com>
2020-01-22 11:44:17 -0800

    add boxes.js and update file with available box products

---
+++ data/boxes.js
@@ -0,0 +1,81 @@
+const averageBox = {
+    id: 21,
+    name: 'Average Box',
+    image: 'averagebox.png',
+    color: 'brown',
+    description: 'It\'s an average box.',
+    category: 'boxes',
+    price: 5.00
+};
+
+const mediumBox = {
+    id: 24,
+    name: 'Medium Box',
+    image: 'mediumbox.png',
+    color: 'brown',
+    description: 'It\'s a medium box.',
+    category: 'boxes',
+    price: 9.00
+};
+
+const largeBox = {
+    id: 43,
+    name: 'Large Box',
+    image: 'largebox.png',
+    color: 'brown',
+    description: 'It\'s a large box.',
+    category: 'boxes',
+    price: 14.00
+};
+
+const longBox = {
+    id: 38,
+    name: 'Long Box',
+    image: 'longbox.png',
+    color: 'brown',
+    description: 'It\'s a long box.',
+    category: 'boxes',
+    price: 17.00
+};
+
+const smallBox = {
+    id: 1,
+    name: 'Small Box',
+    image: 'smallbox.png',
+    color: 'brown',
+    description: 'Not to be confused with smallpox.',
+    category: 'boxes',
+    price: 4.00
+};
+
+const flatBox = {
+    id: 101,
+    name: 'Flat Box',
+    image: 'flatbox.png',
+    color: 'brown',
+    description: 'For like, TV\'s and stuff.',
+    category: 'boxes',
+    price: 13.00
+};
+
+const donutBox = {
+    id: 21,
+    name: 'Donut Box',
+    image: 'donutbox.png',
+    color: 'pink',
+    description: 'No one really knows what this box is used for.',
+    category: 'boxes',
+    price: 27.00
+};
+
+const boxes = [
+    smallBox,
+    mediumBox,
+    largeBox,
+    averageBox,
+    longBox,
+    flatBox,
+    donutBox
+];
+
+export default boxes;
