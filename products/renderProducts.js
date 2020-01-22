2e19cc63c9dccd963a81d7cb14170fcc1f70be78
Logan Scott <logantscott@gmail.com>
2020-01-22 11:40:39 -0800

    update render js for basic product output layout/hierarchy

---
+++ products/renderProducts.js
@@ -0,0 +1,40 @@
+const container = document.getElementById('product');
+
+function renderbox(box) {
+    const li = document.createElement('li');
+
+    const h3 = document.createElement('h3');
+    h3.textContent = box.name;
+    li.appendChild(h3);
+
+    const img = document.createElement('img');
+    img.src = '../assets/' + box.image;
+    img.alt = box.name + ' image';
+    li.appendChild(img);
+
+    const div = document.createElement('div');
+    div.className = 'price';
+    li.appendChild(div);
+
+
+    const salespan = document.createElement('span');
+    const pricespan = document.createElement('span');
+    if (box.sale) {
+        salespan.textContent = box.sale + '&nbsp;&nbsp;';
+        div.appendChild(salespan);
+    }
+    const usd = '$' + box.price.toFixed(2);
+    pricespan.textContent = usd;
+    div.appendChild(pricespan);
+
+    const p = document.createElement('p');
+    p.className = 'description';
+    li.appendChild(p);
+    
+    const button = document.createElement('button');
+    button.textContent = 'Add to Cart';
+    button.value = box.id;
+    p.appendChild(button);
+
+    return li;
+}
