04ea5ff8b9164efe1384f8b1159cd2bf2ffac98f
Logan Scott <logantscott@gmail.com>
2020-01-23 11:38:02 -0800

    add calcLineItem func and TDD

--- common/utils.js
+++ common/utils.js
@@ -1,5 +1,10 @@
-export default function findById(cartItemID, boxesArray) {
+export function findById(cartItemID, boxesArray) {
     let match;
     boxesArray.forEach(boxItem => {if (cartItemID === boxItem.id) match = boxItem;});
     return match;
+}
+
+export function calcLineItem(price, quantity) {
+    const subTotal = Math.round((price * quantity) * 100) / 100;
+    return subTotal;
 }
--- test/boxtest.js
+++ test/boxtest.js
@@ -1,7 +1,8 @@
 // IMPORT MODULES under test here:
 // import example from '../src/example.js';
-import findById from '../common/utils.js';
+import { findById, calcLineItem } from '../common/utils.js';
 import boxes from '../data/boxes.js';
+import cart from '../data/cart.js';
 
 const test = QUnit.test;
 
@@ -25,4 +26,17 @@ test('test findById', function(assert) {
     //Assert
     // Make assertions about what is expected valid result
     assert.equal(JSON.stringify(average), JSON.stringify(expect));
+});
+
+test('test calcLineItem', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const expect = 27.33;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const calc = calcLineItem(9.11, 3);
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.equal(calc, expect);
 });
